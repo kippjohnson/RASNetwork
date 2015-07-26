73a3821e6997b5c4db8941342a1006162d0631ad
Kipp Johnson <kipp.william.johnson@gmail.com>
2015-07-26 15:22:01 -0400

    removing network, subnetwork

--- NetworkData/Networks/annotated/run_annotator.sh
+++
@@ -1,9 +0,0 @@
-python ../../../Code/network_annotater.py ../PF_AD_links_for_cytoscape.txt
-python ../../../Code/network_annotater.py ../PF_normal_links_space_delimited.txt
-python ../../../Code/network_annotater.py ../delimited_blood_network.txt
-python ../../../Code/network_annotater.py ../delimited_pan_intestine_network.txt
-python ../../../Code/network_annotater.py ../delimited_risk_network.txt
-python ../../../Code/network_annotater.py ../ileum_links_space_delimited.txt
-python ../../../Code/network_annotater.py ../omental_links_space_delimited.txt
-
-mv ../*anno*txt .
--- NetworkData/Subnetworks/get_nodes.sh
+++
@@ -1,7 +0,0 @@
-#rm -f keydriver_directed.xls # delete if exists
-#touch keydriver_directed.xls # create keydriver.xls
-
-# Cat every file into keydriver.xls
-for f in ./*deg*/*.sif; do
-    python get_unique_nodes.py $f >> "$f".nodes ;
-done
